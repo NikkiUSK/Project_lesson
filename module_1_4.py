22f6044fba48c46c303405cf951f4b3c3c0eacd0
NikkiUSK <nikkius769@gmail.com>
2024-12-09 18:40:48 +0300

    finished task module 1.4 (1.12)

---
+++ module_1_4.py
@@ -0,0 +1,14 @@
+my_string = input('Введите текст: ')
+print(my_string)
+print(len(my_string))
+
+print(my_string.upper())
+
+print(my_string.lower())
+
+print(my_string.replace(' ', ''))
+print(''.join(my_string.split()))
+
+print(my_string[0])
+
+print(my_string[-1])
